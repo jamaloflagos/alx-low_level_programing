de80cd289383a26b27a6cb368e8ad622a8a73f6d
jamaloflagos <toyinjamal@gmail.com>
2023-09-26 02:09:08 +0100

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strstr- Search for a string
@@ -29,5 +28,5 @@ char *_strstr(char *haystack, char *needle)
 		haystack++;
 	}
 
-	return (NULL);
+	return (0);
 }
