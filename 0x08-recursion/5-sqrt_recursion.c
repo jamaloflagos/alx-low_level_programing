595da86cd841fbae13f9bcae2643dcbabe63445b
jamaloflagos <toyinjamal@gmail.com>
2023-09-29 03:32:36 +0100

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,9 +12,10 @@ int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (0);
-	if (n < 0)
+	else if (n < 0)
 		return (-1);
-	if (n == 1)
+	else if (n == 1)
 		return (1);
-	return (power_operation(n, 2));
+	else
+		return (power_operation(n, 2));
 }
